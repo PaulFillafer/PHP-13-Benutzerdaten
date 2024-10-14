2f84aabd04bd00483be9bcb443c26b4c67560b64
Paul Fillafer <pfillafer@tsn.at>
2024-10-14 20:40:10 +0200

    . Commit detail.php hat implementierung aber funktioniert immer noch nicht

--- detial.php
+++ detial.php
@@ -1,8 +1,13 @@
 <?php
-require 'index.php';
+
 require 'func.php';
 
+$allData=getAllData();
+for($i = 0; $i < count($allData); $i++){
+    $tableData=$allData[$i];
+    global $gloId;
+    $gloId=$tableData["id"];
+    echo '<tr>'.'<td>'.$tableData['firstname'].'</td>'.'<td>'.$tableData['lastname'].'</td>'.'<td>'.$tableData['birthdate'].'</td>'.'</tr>';
+}
 
-global $gloId;
-
-getDataPerID($gloId);
+?>
